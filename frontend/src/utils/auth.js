17bfeb3746deadaf8a9db6f5102b7758a11555ae
Daria Krushelnitskaya <dasha_fr_russia@mail.ru>
2023-06-01 20:26:01 +0300

    controller Users-2

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = "https://api-krylatka.nomoredomains.rocks";
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = "https://api-krylatka.nomoredomains.rocks";
 
 
 const checkResponse = (response) =>
